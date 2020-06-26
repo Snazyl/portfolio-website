7ea19a77f2f94db66afab6b030a8d3ac0c53aa39
Ilia <IliaPetrovs@hotmail.co.uk>
2020-06-26 13:03:05 +0100

    responsive navbar fully implemented

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -15,35 +15,39 @@ const Navbar = () => {
   };
 
   return (
-    // <div className="nav-contain">
-    <nav>
-      <div>
-        <h1 className="header-name">
-          LEE FOSTER <span className="voice">Voice Actor</span>
-        </h1>
-      </div>
-      <div>
-        <ul className="navbar">
-          <li className="link1">
-            <a className="active-page" href="#">
-              HOME
-            </a>
-          </li>
-          <li className="link2">
-            <a href="#">TESTIMONIALS</a>
-          </li>
-          <li className="link3">
-            <a href="#">CONTACT</a>
-          </li>
-        </ul>
-      </div>
-      <div className="hamburger">
-        <a href="javascript:void(0);" class="icon" onClick={hamburger}>
-          <i class="fa fa-bars"></i>
-        </a>
-      </div>
-    </nav>
-    // </div>
+    <header>
+      <nav>
+        <div>
+          <h1 className="header-name">
+            LEE FOSTER <span className="voice">Voice Actor</span>
+          </h1>
+        </div>
+        <div>
+          <input id="nav-toggle" type="checkbox" className="nav-toggle" />
+          <ul className="navbar">
+            <li className="link1">
+              <a className="active-page" href="#">
+                HOME
+              </a>
+            </li>
+            <li className="link2">
+              <a href="#">TESTIMONIALS</a>
+            </li>
+            <li className="link3">
+              <a href="#">CONTACT</a>
+            </li>
+          </ul>
+        </div>
+        {/* <div className="hamburger">
+          <a href="javascript:void(0);" class="icon" onClick={hamburger}>
+            <i class="fa fa-bars"></i>
+          </a>
+        </div> */}
+      </nav>
+      <label htmlFor="nav-toggle" className="nav-toggle-label">
+        <span></span>
+      </label>
+    </header>
   );
 };
 

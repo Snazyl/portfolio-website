076aa7743dcdf5a74c07245c30a64a42cc52b577
Snazyl <iliapetrovs@hotmail.co.uk>
2020-09-09 14:57:22 +0100

    front end tidied up, contact form turned to a class component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,41 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Contact = () => {
-  return (
-    <div className="contact_container">
-      <div className="contact">
-        <h1 className="contact_header">Contact me</h1>
-        <div className="contact_form">
-          <div className="form_wrapper">
-            <form>
-              <label for="name">Name</label>{" "}
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Name..."
-              ></input>
-              <label for="email">Email</label>{" "}
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email..."
-              ></input>
-              <label for="message">Message</label>{" "}
-              <textarea
-                style={{ height: "200px" }}
-                type="textarea"
-                id="message"
-                name="message"
-                placeholder="Your message..."
-              ></textarea>
-            </form>
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      message: "",
+    };
+  }
+
+  render() {
+    const { name, email, message } = this.state;
+
+    return (
+      <div className="contact_container">
+        <div className="contact">
+          <h1 className="contact_header">Contact me</h1>
+          <div className="contact_form">
+            <div className="form_wrapper">
+              <form method="POST">
+                <input
+                  className="no_border"
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={this.handleChange}
+                  placeholder="Your name..."
+                ></input>
+                <input
+                  className="no_border"
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={this.handleChange}
+                  placeholder="Your email..."
+                ></input>
+                <textarea
+                  className="no_border"
+                  style={{ minHeight: "40%" }}
+                  type="textarea"
+                  id="message"
+                  name="message"
+                  value={message}
+                  onChange={this.handleChange}
+                  placeholder="Your message..."
+                ></textarea>
+                <input
+                  onSubmit={this.handleSubmit}
+                  class="submit"
+                  type="submit"
+                  value="Submit"
+                ></input>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({ ...this.state, [name]: value });
+  };
+}
 
 export default Contact;
